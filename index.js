12214884f42434d1e106baa1b58299133402ac4a
Finderstein <findershtein@gmail.com>
2021-11-30 23:57:30 +0200

    Routes bug fix

--- index.js
+++ index.js
@@ -25,14 +25,6 @@ app.get("/", (req, res) => {
 	res.sendFile(path.join(__dirname, "public/index.html"));
 });
 
-app.get("/login", (req, res) => {
-	res.json({ message: "Welcome to my games finder." });
-});
-
-app.get("/register", (req, res) => {
-	res.json({ message: "Welcome to my games finder." });
-});
-
 // routes
 app.use("/api/auth", authRouter);
 app.use("/api/column", columnRouter);
